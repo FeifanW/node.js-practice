53714c93b75d515d3be240f9288254f9c58474e1
FeifanW <qihang791@163.com>
2022-03-20 23:05:08 +0800

    练习文件写入

--- fs.js
+++ fs.js
@@ -11,9 +11,23 @@ const fs = require('fs')
 // 参数2：读取文件的编码格式，一般默认指定utf-8
 // 参数3：回调函数，拿到读取失败和成功的结果 err dataStr
 //        读取成功err为null，读取失败err为错误对象，dataStr的值为undefined
-fs.readFile('./fs.txt','utf8',function(err, dataStr){
+// fs.readFile('./fs.txt','utf8',function(err, dataStr){
+//   if(err){
+//     return console.log('读取文件失败！' + err.message);
+//   }
+//   console.log('读取文件成功'+ dataStr);
+// })
+
+// 二、fs.writeFile(file, data[,options], callback)
+// 参数1：文件路径字符串
+// 参数2：要写入的内容
+// 参数3：可选参数，表示文件写入格式
+// 参数4：文件写入之后的回调函数
+fs.writeFile('./fs.txt','abcd', function(err){
+  // 文件写入成功返回 null
+  // console.log(err);
   if(err){
-    return console.log('读取文件失败！' + err.message);
+    return console.log('文件写入失败！' + err.message);
   }
-  console.log('读取文件成功'+ dataStr);
+  console.log('文件写入成功');
 })

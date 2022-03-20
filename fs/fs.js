8eaae7e84e99e0d4b8230b1cb854dc6738d9bdfa
FeifanW <qihang791@163.com>
2022-03-21 00:18:31 +0800

    fs练习-整理成绩

--- fs/fs.js
+++ fs/fs.js
@@ -23,11 +23,37 @@ const fs = require('fs')
 // 参数2：要写入的内容
 // 参数3：可选参数，表示文件写入格式
 // 参数4：文件写入之后的回调函数
-fs.writeFile('./fs.txt','abcd', function(err){
+// fs.writeFile('./fs.txt','abcd', function(err){
   // 文件写入成功返回 null
   // console.log(err);
+//   if(err){
+//     return console.log('文件写入失败！' + err.message);
+//   }
+//   console.log('文件写入成功');
+// })
+
+
+// 三、练习处理成绩
+fs.readFile('./成绩.txt','utf8',function(err, dataStr){
   if(err){
-    return console.log('文件写入失败！' + err.message);
+    return console.log('读取文件失败！' + err.message);
   }
-  console.log('文件写入成功');
+  console.log('读取文件成功'+ dataStr);
+
+  const arrOld = dataStr.split(' ')    // 原来的成绩按照空格分割
+  const arrNew = []
+  arrOld.forEach(item => {
+    arrNew.push(item.replace('=','：'))
+  })
+  const newStr = arrNew.join('\r\n')   // join会把数组每项拼成一个字符串，传的参数就是分隔符
+  console.log("新字符串",newStr);
+
+  // 调用fs.writeFile()把处理完毕的成绩保存到新文件中
+  fs.writeFile('./成绩-ok.txt',newStr, function(err) {
+    if(err){
+      return console.log('写入文件失败！' + err.message);
+    }
+    console.log('成绩写入成功！');
+  })
+
 })

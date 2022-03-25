3ecb8d40dc037b50f3131c6f1bc7d48cad026c3a
FeifanW <qihang791@163.com>
2022-03-26 00:21:04 +0800

    练习http模块

---
+++ http/req请求对象.js
@@ -0,0 +1,19 @@
+// 1.导入http模块
+const http = require('http')
+// 2.创建web服务器实例
+const server = http.createServer()
+// 3.为服务器实例绑定 request 事件，监听客户端的请求
+server.on('request', (req,res)=>{
+  // req.url 是客户端请求的url地址
+  const url = req.url
+  // req.method 是客户端请求的method类型
+  const method = req.method
+  const str = `你请求的url地址是 ${url}, 请求的类型是 ${method}`
+  console.log(str);
+  // 调用res.end() 向客户端响应一些内容
+  res.end(str)
+})
+// 4.启动服务器
+server.listen(80, function(){
+  console.log('server running at http://127.0.0.1:80');
+})

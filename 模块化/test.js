a2f61d3f87b45e1c6046c97c9adadc50872795cb
FeifanW <qihang791@163.com>
2022-03-28 00:25:45 +0800

    学习CommonJS规范

--- 模块化/test.js
+++ 模块化/test.js
@@ -5,4 +5,11 @@
 const custom = require('./模块作用域')
 console.log(custom);
 
-console.log("module", module);
+// console.log("module", module);
+// console.log("exports和module.exports是否相等",exports === module.exports);
+
+// Node.js遵循了CommonJS模块化规范
+// CommonJS模块化规范：
+// 1.每个模块内部，module变量代表当前模块
+// 2.module变量是一个对象，它的exports属性（即module.exoprts）是对外的接口
+// 3.加载某个模块，其实是加载改模块的module.exoprts属性，require()方法用于加载模块
--- 模块化/模块作用域.js
+++ 模块化/模块作用域.js
@@ -1,4 +1,10 @@
 const username = '张三'
-function sayHello() {
+module.exports.sayHello = function() {
   console.log('大家好我是' + username);
 }
+module.exports.username = 'feifan'
+exports.today = '20220328'
+// 使用require()导入模块时导入的结果永远以module.exports指向的对象为准
+// module.exports = {
+//   test:"新的对象"
+// }

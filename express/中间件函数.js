e4129c9b5cd7224161f7dff36f7a28f26bb3f925
FeifanW <qihang791@163.com>
2022-04-04 21:42:55 +0800

    练习自定义中间件

--- express/中间件函数.js
+++ express/中间件函数.js
@@ -2,18 +2,21 @@ const express = require('express')
 const app = express()
 
 // 导入解析表单数据的第三方中间件
-const parser = require('body-parser')
+// const parser = require('body-parser')
 
-app.use(parser.urlencoded({extended: false}))
+const customBodyParser = require('./custom-body-parser')
+
+app.use(customBodyParser)
+// app.use(parser.urlencoded({extended: false}))
 
 // 定义一个最简单的中间件函数
-const mw = function(req, res, next){
-  console.log('这是最简单的中间件函数');
-  const time = Date.now()
-  req.startTime = time
-  // 把流转关系转交给下一个中间件
-  next()
-}
+// const mw = function(req, res, next){
+//   console.log('这是最简单的中间件函数');
+//   const time = Date.now()
+//   req.startTime = time
+//   // 把流转关系转交给下一个中间件
+//   next()
+// }
 
 // 将mw注册为全局生效的中间件
 // app.use(mw)

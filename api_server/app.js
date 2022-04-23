a8c7bc3aa4e8b4731d9824017bac50839d9cc77b
FeifanW <qihang791@163.com>
2022-04-24 07:16:25 +0800

    初始化路由模块

--- api_server/app.js
+++ api_server/app.js
@@ -8,7 +8,11 @@ const cors = require('cors')
 app.use(cors())
 
 // 配置解析表单数据的中间件
-app.use(express.urlencoded({extented: falsecd}))
+app.use(express.urlencoded({extented: false}))
+
+// 导入并使用用户路由模块
+const userRouter = require("./router/user")
+app.use('/api', userRouter)
 
 // 启动服务器
 app.listen(3007, ()=>{
